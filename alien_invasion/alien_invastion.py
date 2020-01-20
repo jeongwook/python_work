a7e09a715054c3f390f1f533f4548b3def49d2e0
jeongwook <jeongwook.yu@utexas.edu>
2020-01-20 13:01:05 -0800

    Check for collisions and remove items appropriately. Create new fleet if all aliens destroyed.

--- alien_invasion/alien_invastion.py
+++ alien_invasion/alien_invastion.py
@@ -13,10 +13,13 @@ def run_game():
 		(ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
 
-	# Make a ship
+	# Make a ship, a group of bullets, and a group of aliens.
 	ship = Ship(ai_settings, screen)
-	# Make a group to store bullets in.
 	bullets = Group()
+	aliens = Group()
+
+	# Crate the fleet of aliens.
+	gf.create_fleet(ai_settings, screen, ship, aliens)
 
 	# Start the main loop for the game
 	while True:
@@ -27,7 +30,8 @@ def run_game():
 
 		# Redraw the screen and flip to new screen 
 		# during each pass through the loop
-		gf.update_bullets(bullets)
-		gf.update_screen(ai_settings, screen, ship, bullets)
+		gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+		gf.update_aliens(ai_settings, aliens)
+		gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
